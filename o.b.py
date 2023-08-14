edfeb05611e6dc54fecfe7db6aa1d5d183d1137e
reza72rg <133125310+reza72rg@users.noreply.github.com>
2023-08-14 20:42:15 +0330

    Update o.b.py

--- o.b.py
+++ o.b.py
@@ -1,18 +1,16 @@
 class Employee:
     def __init__(self, name, age):
+        """
+        Initialize an Employee object with a name and age.
+        """
         self.name = name
         self.age = age
 
-employees = []
-
 num_employees = int(input("Enter number of employees: "))
 
-for i in range(num_employees):
-    name = input("Enter name of employee {}: ".format(i+1))
-    age = input("Enter age of employee {}: ".format(i+1))
-    employee = Employee(name, age)
-    employees.append(employee)
+# Create a list of Employee objects using a list comprehension
+employees = [Employee(input("Enter name of employee {}: ".format(i+1)), input("Enter age of employee {}: ".format(i+1))) for i in range(num_employees)]
 
+# Print the name and age of each employee
 for employee in employees:
     print("{} is {} years old".format(employee.name, employee.age))
-    
